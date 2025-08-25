b8c743d68d5273de3e579fca0a19a5b293b354e5
Luis Monge <luisdi28ms@gmail.com>
2025-08-24 20:22:58 -0600

    add files system

--- src/transcriber.py
+++ src/transcriber.py
@@ -2,4 +2,5 @@ import whisper
 
 model = whisper.load_model("base.en")
 result = model.transcribe("VMP7679265734.mp3")
-print(result["text"])
+with open("files/output/VMP7679265734.txt", "w") as file:
+    file.write(result["text"])
